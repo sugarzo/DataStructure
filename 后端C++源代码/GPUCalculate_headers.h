015d5c579f3c83f6aa59719924e3f75af3eae444
ordzzzzzz <74815734+sugarzo@users.noreply.github.com>
2022-05-16 00:54:50 +0800

    Delete 后端C++源代码 directory

--- 后端C++源代码/GPUCalculate_headers.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define _SILENCE_AMP_DEPRECATION_WARNINGS
-#include <amp.h>
-#include <amp_graphics.h>
-#include <amp_math.h>
-//using namespace concurrency;
-//using namespace Concurrency::precise_math;
